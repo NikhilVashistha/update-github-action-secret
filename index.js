282f7a8c8217da8a670bcf021315ad4d02dbd282
emin <emin@grbo.space>
2024-09-19 18:01:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ const updateGithubSecret = async () => {
         },
       );
 
-      if(updateSecretRequest.status && updateSecretRequest.status == 200) {
+      if(updateSecretRequest.status || updateSecretRequest.status == 200) {
         core.setOutput('result', '✅ Github Action Secret Updated Successfully!');
       } else {
         core.setFailed('❌ Failed to update repository action secret!');
